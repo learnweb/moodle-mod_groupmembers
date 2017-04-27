bdb927ba1e1aa25a84497daae349b2c045876705
Dennis Riehle <webmaster@riehle-web.com>
2017-04-27 16:45:00 +0200

    added German translation

---
+++ lang/de/groupmembers.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of a plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin language strings
+ *
+ * @package    mod_groupmembers
+ * @copyright  2017 Dennis M. Riehle, WWU Münster
+ * @copyright  2017 Jan C. Dageförde, WWU Münster
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['modulename'] = 'Gruppenmitglieder';
+$string['modulenameplural'] = 'Gruppenmitglieder';
+$string['modulename_help'] = 'Mit dem Gruppenmitglieder-Modul lassen sich die Mitglieder aller Gruppen einer Gruppierung auflisten.';
+$string['pluginname'] = 'Gruppenmitglieder';
+$string['pluginadministration'] = 'Gruppenmitglieder-Administration';
+$string['groupmembers:addinstance'] = 'Neue Mitgliederliste hinzufügen';
+$string['groupmembers:view'] = 'Gruppenmitglieder anzeigen';
+$string['groupmembersname'] = 'Name der Mitgliederliste';
+$string['description'] = 'Beschreibung';
+$string['listgrouping'] = 'Gruppierung';
+$string['showgroups'] = 'Anzuzeigende Gruppen';
+$string['showgroups:all'] = 'Alle Gruppen der Gruppierung';
+$string['showgroups:own'] = 'Nur die eigene Gruppe';
+$string['showemail'] = 'E-Mail-Adressen anzeigen';
+$string['showemail:no'] = 'Nein, nur interne Nachrichten';
+$string['showemail:owngroup'] = 'Nur für die eigene Gruppe';
+$string['showemail:allgroups'] = 'Für alle Gruppen';
+$string['user:fullname'] = 'Vorname / Nachname';
+$string['user:contact'] = 'Kontakt';
+$string['noowngroupsavailable'] = 'Du bist derzeit keiner Gruppe zugewiesen.';
+$string['nogroupsavailable'] = 'Es sind derzeit keine Gruppen zur Anzeige hinterlegt.';
+
