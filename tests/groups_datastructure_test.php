df1852cdd931fd0c9d21ecce35d30998c0c2a824
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-05-05 10:37:24 +0200

    Finished refactoring PHPUnit tests

--- tests/groups_datastructure_test.php
+++ tests/groups_datastructure_test.php
@@ -35,7 +35,7 @@ class mod_groupmembers_groups_datastructure_testcase extends advanced_testcase {
         $this->resetAfterTest();
         $dg = static::getDataGenerator();
 
-        list($courseid, $userid, $groupid) = static::prepare_nogrouping($dg);
+        list($courseid, $userid, $groupid) = static::prepare_basic_data($dg);
 
         static::assertEmpty(\mod_groupmembers\groups::get_groups_and_members($courseid, 0, $userid, true));
 
@@ -55,7 +55,7 @@ class mod_groupmembers_groups_datastructure_testcase extends advanced_testcase {
     public function test_only_own_withgrouping() {
         $this->resetAfterTest();
         $dg = static::getDataGenerator();
-        list($courseid, $userid, $groupid, $groupingid) = $this->prepare_nogrouping($dg);
+        list($courseid, $userid, $groupid, $groupingid) = $this->prepare_basic_data($dg);
 
         $grouping2 = $dg->create_grouping(['courseid' => $courseid]);
 
@@ -79,7 +79,7 @@ class mod_groupmembers_groups_datastructure_testcase extends advanced_testcase {
     public function test_all_nogrouping() {
         $this->resetAfterTest();
         $dg = static::getDataGenerator();
-        list($courseid, $userid, $groupid) = $this->prepare_nogrouping($dg);
+        list($courseid, $userid, $groupid) = $this->prepare_basic_data($dg);
 
         $res1 = \mod_groupmembers\groups::get_groups_and_members($courseid, 0, $userid, false);
         static::assertCount(2, $res1);
@@ -110,42 +110,29 @@ class mod_groupmembers_groups_datastructure_testcase extends advanced_testcase {
     public function test_all_withgrouping() {
         $this->resetAfterTest();
         $dg = static::getDataGenerator();
+        list($courseid, $userid) = $this->prepare_basic_data($dg);
 
-        // Create course.
-        $course = $dg->create_course();
+        // Create additional groups and groupings.
+        $group3 = $dg->create_group(['courseid' => $courseid]);
+        $group4 = $dg->create_group(['courseid' => $courseid]);
+        $grouping2 = $dg->create_grouping(['courseid' => $courseid]);
 
-        // Create users and enrol.
-        $user1 = $dg->create_user();
-        $user2 = $dg->create_user();
-        $dg->enrol_user($user1->id, $course->id);
-        $dg->enrol_user($user2->id, $course->id);
-
-        // Create groups and groupings.
-        $group1 = $dg->create_group(['courseid' => $course->id]);
-        $group2 = $dg->create_group(['courseid' => $course->id]);
-        $group3 = $dg->create_group(['courseid' => $course->id]);
-        $group4 = $dg->create_group(['courseid' => $course->id]);
-        $grouping1 = $dg->create_grouping(['courseid' => $course->id]);
-        $grouping2 = $dg->create_grouping(['courseid' => $course->id]);
-        $dg->create_grouping_group(['groupingid' => $grouping1->id, 'groupid' => $group1->id]);
-        $dg->create_grouping_group(['groupingid' => $grouping1->id, 'groupid' => $group2->id]);
-
-        static::assertEmpty(\mod_groupmembers\groups::get_groups_and_members($course->id, $grouping2->id, $user1->id, false));
+        static::assertEmpty(\mod_groupmembers\groups::get_groups_and_members($courseid, $grouping2->id, $userid, false));
 
         // Add group to grouping.
         $dg->create_grouping_group(['groupingid' => $grouping2->id, 'groupid' => $group3->id]);
         $dg->create_grouping_group(['groupingid' => $grouping2->id, 'groupid' => $group4->id]);
 
-        $res1 = \mod_groupmembers\groups::get_groups_and_members($course->id, $grouping2->id, $user1->id, false);
+        $res1 = \mod_groupmembers\groups::get_groups_and_members($courseid, $grouping2->id, $userid, false);
         static::assertCount(2, $res1);
         foreach ($res1 as $group) {
             self::assertFalse($group['ismember']);
         }
 
         // Add user to group.
-        $dg->create_group_member(['userid' => $user1->id, 'groupid' => $group3->id]);
+        $dg->create_group_member(['userid' => $userid, 'groupid' => $group3->id]);
 
-        $res2 = \mod_groupmembers\groups::get_groups_and_members($course->id, $grouping2->id, $user1->id, false);
+        $res2 = \mod_groupmembers\groups::get_groups_and_members($courseid, $grouping2->id, $userid, false);
         static::assertCount(2, $res2);
         foreach ($res2 as $group) {
             if ($group['group']->id == $group3->id) {
@@ -156,7 +143,19 @@ class mod_groupmembers_groups_datastructure_testcase extends advanced_testcase {
         }
     }
 
-    private static function prepare_nogrouping($dg) {
+    /**
+     * Prepares a general testing environment that is relevant for this test case, consisting of
+     * - 2 users, both enrolled in
+     * - 1 course, that contains
+     * - 1 grouping, which consists of
+     * - 2 groups.
+     *
+     * Users are not members of any groups.
+     *
+     * @param $dg testing_data_generator Data generator
+     * @return array [ID of course, ID of first user, ID of first group, ID of grouping]
+     */
+    private static function prepare_basic_data(testing_data_generator $dg) {
         // Create course.
         $course = $dg->create_course();
 
@@ -169,10 +168,10 @@ class mod_groupmembers_groups_datastructure_testcase extends advanced_testcase {
         // Create groups and groupings.
         $group1 = $dg->create_group(['courseid' => $course->id]);
         $group2 = $dg->create_group(['courseid' => $course->id]);
-        $grouping1 = $dg->create_grouping(['courseid' => $course->id]);
-        $dg->create_grouping_group(['groupingid' => $grouping1->id, 'groupid' => $group1->id]);
-        $dg->create_grouping_group(['groupingid' => $grouping1->id, 'groupid' => $group2->id]);
+        $grouping = $dg->create_grouping(['courseid' => $course->id]);
+        $dg->create_grouping_group(['groupingid' => $grouping->id, 'groupid' => $group1->id]);
+        $dg->create_grouping_group(['groupingid' => $grouping->id, 'groupid' => $group2->id]);
 
-        return [$course->id, $user1->id, $group1->id, $grouping1->id];
+        return [$course->id, $user1->id, $group1->id, $grouping->id];
     }
 }
