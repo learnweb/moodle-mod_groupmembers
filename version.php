976f844e385366603a8f6046b4cb66760b1cef4f
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-10-24 13:31:40 +0200

    Release v3.4-r2

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.4-r1';
-$plugin->version   = 2017102300;         // The current module version (Date: YYYYMMDDXX)
+$plugin->release   = 'v3.4-r2';
+$plugin->version   = 2017102400;         // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016120500;         // Requires this Moodle version
 $plugin->component = 'mod_groupmembers'; // Full name of the plugin (used for diagnostics).
