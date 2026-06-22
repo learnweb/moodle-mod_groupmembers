94affbf73f15566b96047c428a219df28eaaa4ed
lenaherf <lherfeld@uni-muenster.de>
2026-06-22 16:08:03 +0200

    new version 5.2

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v5.0-r2';
-$plugin->version   = 2025091700;         // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022112800;         // Requires 4.1+ Moodle version.
+$plugin->release   = 'v5.2-r0';
+$plugin->version   = 2026061700;         // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2024100712;         // Requires 4.1+ Moodle version.
 $plugin->component = 'mod_groupmembers'; // Full name of the plugin (used for diagnostics).
